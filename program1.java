b1dbfe9bad5447dff08b8dfc403876026c0f354c
karnamanu <manojbm497@gmail.com>
2025-09-18 02:13:56 +0530

    characters printing

--- program1.java
+++ program1.java
@@ -1,8 +1,15 @@
 public class program1 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
-            for (int j = i; j <= 4; j++) {
-                System.out.print("*");
+        char ch = 'A';
+        int row = 5;
+
+        for (int i = 1; i<= row; i++) {
+           
+            for (int j =1; j <= i; j++) {
+                System.out.print(ch +" ");
+                ch++;
+                
+                
                 
             }
             System.out.println();
