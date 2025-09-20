11001479b7bbec08899ad2eb51e8663cdb8b9abc
karnamanu <manojbm497@gmail.com>
2025-09-20 15:51:14 +0530

    solid rhombus printed

---
+++ solidrhombus.java
@@ -0,0 +1,20 @@
+
+
+public class solidrhombus {
+    public static void solidRhombus(int n){
+        for(int i = 1; i <=n; i++){
+            //spaces
+            for(int j = 1; j<= (n-i);j ++){// if i make n+i then it will become hollow rhombus
+                System.out.print(" ");
+            }
+            //stars 
+            for(int j= 1; j<= n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        solidRhombus(5);
+    }
+}
